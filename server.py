8d1b7dd4c5e0ae7c925dc3702dd93daa950e40b1
harshit <sonu10081987@gmail.com>
2025-08-05 16:17:14 +0530

    fix bind to 0.0.0.0 again render 502

--- server.py
+++ server.py
@@ -12,5 +12,5 @@ def get_data():
     return jsonify({"message": "This is data from your Flask API!"})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Render sets this automatically
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
