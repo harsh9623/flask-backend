d6a44dba1fcb18d7f1c34e1c37ebce9e506e3450
harshit <sonu10081987@gmail.com>
2025-08-05 16:07:17 +0530

    fix bind to 0.0.0.0.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,5 @@ def get_data():
     return jsonify({"message": "This is data from your Flask API!"})
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # use the port Render provides
+    app.run(host='0.0.0.0', port=port)
